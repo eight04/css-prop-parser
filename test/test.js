7ded4072660a6f22de68ab66b094f0080067ec55
eight04 <eight04@gmail.com>
2025-12-07 02:47:07 +0800

    Add: more test

--- test/test.js
+++ test/test.js
@@ -33,8 +33,38 @@ test("ignore unknown func", () => {
   assert.deepEqual(result, expected);
 });
 
+// NOTE: parseCommaList does consume the closing paren but doesn't check for it
+test("Throw on missing paren", {skip: true}, () => {
+  const input = "url(image.png, inherit";
+  assert.throws(() => {
+    parseCommaList( {input, lastIndex: 0});
+  });
+});
+
+test("unquote string", () => {
+  const input = '"ima\\22ge1.png", \'image2.png\'';
+  const result = [];
+  parseCommaList(
+    {input, lastIndex: 0},
+    {
+      string: (_, s) => {
+        result.push(s);
+      }
+    }
+  );
+  const expected = ['ima"ge1.png', "image2.png"];
+  assert.deepEqual(result, expected);
+});
+
+test("unclosed quote", () => {
+  const input = '"image1.png, image2.png\'';
+  assert.throws(() => {
+    parseCommaList( {input, lastIndex: 0});
+  });
+})
+
 test("parseBackgroundImage", () => {
-  const input = 'url("image1.png"), image-set(url("image\\"2.png") 1x, "image3.png" 2x)';
+  const input = 'url("image1.png"), image-set(url("image\\"2.png") 1x, "image3.png" 2x), url( foo.png )';
   const result = parseBackgroundImage(input);
   const expected = [
     {
@@ -51,8 +81,12 @@ test("parseBackgroundImage", () => {
         {
           url: "image3.png",
           resolution: "2x"
-        }
+        },
       ]
+    },
+    {
+      type: "url",
+      url: "foo.png"
     }
   ];
   assert.deepEqual(result, expected);
